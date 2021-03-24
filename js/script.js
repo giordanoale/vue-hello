8bf32a2073c9c20acaa3e15038590d7e974a527a
giordanoale <a.giordano89@gmail.com>
2021-03-24 16:58:21 +0100

    My job here is done

--- js/script.js
+++ js/script.js
@@ -6,5 +6,16 @@ var app = new Vue({
     el: "#root",
     data: {
         message: "Hello World",
+        image: "https://www.icbz4.it/alunnifermi/wp-content/uploads/2019/12/significato-stonks-1024x766.jpg",
+        displayClass: "hide",
+    },
+    methods: {
+        funzione: function(){
+            if (this.displayClass == "hide"){
+                this.displayClass = "block";
+            } else {
+                this.displayClass = "hide";  
+            }
+        }
     }
 })
